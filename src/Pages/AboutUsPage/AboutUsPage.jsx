152f4fed9c389d0bd0c014898f365a1d12d4512d
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-12-28 21:37:18 +0530

    bug fix

--- src/Pages/AboutUsPage/AboutUsPage.jsx
+++ src/Pages/AboutUsPage/AboutUsPage.jsx
@@ -10,6 +10,10 @@ function AboutUsPage(props) {
                 We are a group of developers who
                  are passionate about creating web applications. We are always looking for new projects to work on and new technologies to learn. We are excited to work with you on your next project. 
             </p>
+
+            <span>
+                Hello
+                </span>
         </div>
     );
 }
