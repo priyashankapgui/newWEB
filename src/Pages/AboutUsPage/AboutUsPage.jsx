bd5ef8f1def4d7bfd77514c59375b410b473c580
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-12-28 15:04:31 +0530

    Add the para

--- src/Pages/AboutUsPage/AboutUsPage.jsx
+++ src/Pages/AboutUsPage/AboutUsPage.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function AboutUsPage(props) {
     return (
         <div>
-            <h1>About Us</h1>
+            <h1>About Us
+                
+            </h1>
+            <p>
+                We are a group of developers who
+                 are passionate about creating web applications. We are always looking for new projects to work on and new technologies to learn. We are excited to work with you on your next project. 
+            </p>
         </div>
     );
 }
